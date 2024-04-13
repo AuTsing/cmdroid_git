55a2135109ba26e56da941fafe3073f85abe587e
AuTsing <763025696@qq.com>
2024-04-13 11:08:37 +0800

    finish base logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use git2::Repository;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+
+    let url = args.get(1).unwrap();
+    let into = args.get(2).unwrap();
+
+    println!("Cloning {} => {}", url, into);
+
+    Repository::clone(url, into).unwrap();
+
+    println!("Clone finished.")
 }
